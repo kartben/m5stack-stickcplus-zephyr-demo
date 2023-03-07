1cfe007b052e1dfe1dc5c3de8099fdb3111deba4
Benjamin Cabé <benjamin@zephyrproject.org>
2023-03-07 19:43:22 +0100

    add overlay for the screen
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- app/src/main.c
+++ app/src/main.c
@@ -4,6 +4,7 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/display.h>
 
 #include "app_version.h"
 
@@ -13,7 +14,10 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 void main(void)
 {
 	int ret;
-	const struct device *sensor;
+	const struct device *display_dev;
+
+	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+
 
 	printk("Zephyr Example Application %s\n", APP_VERSION_STR);
 
